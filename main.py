69857821147ef0fe61d7ad9a8aea7c42909af2cc
brs2003 <brs39972@gmail.com>
2025-08-05 13:53:58 +0530

    fix: allow GitHub Pages origin in CORS

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://brs2003.github.io",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
